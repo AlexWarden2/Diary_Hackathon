73e9f50e5c5b6a9aca1a6feda59a2908fe68cd0e
Alex Warden <alex.wardenn@gmail.com>
2023-11-23 11:11:07 +0000

    fixed delete btn and logout

--- api/api.js
+++ api/api.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
-const logRoutes = require('morgan')
+const logger = require('morgan');
 
 const postRouter = require('./routers/post');
 const userRouter = require('./routers/user');
@@ -10,25 +9,25 @@ const api = express();
 
 api.use(cors());
 api.use(express.json());
-api.use(logRoutes('dev'));
+api.use(logger('dev'));
 
-api.get("/", (req, res) => {
-    res.json({
-        name: "Discretion",
-        description: "Send and receive private messages.",
-        endpoints: [
-            'GET  /posts',
-            'GET  /posts/:id',
-            'POST  /posts',
-            'DELETE  /posts/:id',
-            'POST  /users/login',
-            'POST  /users/register',
-            'GET  /users/logout'
-          ]
-    })
-})
+api.get('/', (req, res) => {
+  res.status(200).json({
+    name: 'Discretion',
+    description: 'Send and receive private messages.',
+    endpoints: [
+      'GET  /posts',
+      'GET  /posts/:id',
+      'POST  /posts',
+      'DELETE  /posts/:id',
+      'POST  /users/login',
+      'POST  /users/register',
+      'GET  /users/logout'
+    ]
+  });
+});
 
-api.use("/posts", postRouter);
-api.use("/users", userRouter);
+api.use('/posts', postRouter);
+api.use('/users', userRouter);
 
 module.exports = api;
